71f29c664274fffa16cea3731da299c3a791c15f
Edgars Vasiljevs <edgars@simpleweb.lv>
2023-01-10 15:59:06 +0200

    update

--- functions/pre.sh
+++ functions/pre.sh
@@ -2,7 +2,7 @@
 
 # get wasmedge & wasmedgec
 curl -sSfL https://github.com/WasmEdge/WasmEdge/releases/download/0.11.2/WasmEdge-0.11.2-manylinux2014_x86_64.tar.gz -o ./WasmEdge.tar.gz
-tar --strip-components 2 -xzvf WasmEdge.tar.gz WasmEdge-0.8.2-rc.2-Linux/bin
+tar --strip-components 2 -xzvf WasmEdge.tar.gz WasmEdge-0.11.2-Linux/bin
 rm WasmEdge.tar.gz
 
 # compile all .wasm to .so
