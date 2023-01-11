6c1697abad582effebdd0417f0b8e7d5278861bb
Edgars Vasiljevs <edgars@simpleweb.lv>
2023-01-11 12:29:34 +0200

    update

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,9 @@
-const path = require('path');
-const fs = require('fs');
-const { svg2png, initialize } = require('svg2png-wasm');
 const { Resvg } = require('@resvg/resvg-js')
 
-
-const svg = `<svg viewBox="-8 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm105.6-151.4c-25.9 8.3-64.4 13.1-105.6 13.1s-79.6-4.8-105.6-13.1c-9.8-3.1-19.4 5.3-17.7 15.3 7.9 47.2 71.3 80 123.3 80s115.3-32.9 123.3-80c1.6-9.8-7.7-18.4-17.7-15.3zm-227.9-57.5c-1 6.2 5.4 11 11 7.9l31.3-16.3 31.3 16.3c5.6 3.1 12-1.7 11-7.9l-6-34.9 25.4-24.6c4.5-4.5 1.9-12.2-4.3-13.2l-34.9-5-15.5-31.6c-2.9-5.8-11-5.8-13.9 0l-15.5 31.6-34.9 5c-6.2.9-8.9 8.6-4.3 13.2l25.4 24.6-6.1 34.9zm259.7-72.7l-34.9-5-15.5-31.6c-2.9-5.8-11-5.8-13.9 0l-15.5 31.6-34.9 5c-6.2.9-8.9 8.6-4.3 13.2l25.4 24.6-6 34.9c-1 6.2 5.4 11 11 7.9l31.3-16.3 31.3 16.3c5.6 3.1 12-1.7 11-7.9l-6-34.9 25.4-24.6c4.5-4.6 1.8-12.2-4.4-13.2z"/></svg>`;
-
-let init = false;
-
-function toBuffer(ab) {
-    const buf = Buffer.alloc(ab.byteLength);
-    const view = new Uint8Array(ab);
-    for (let i = 0; i < buf.length; ++i) {
-        buf[i] = view[i];
-    }
-    return buf;
-}
+const svg = `
+    <svg viewBox="-8 0 512 512" xmlns="http://www.w3.org/2000/svg">
+        <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 448c-110.3 0-200-89.7-200-200S137.7 56 248 56s200 89.7 200 200-89.7 200-200 200zm105.6-151.4c-25.9 8.3-64.4 13.1-105.6 13.1s-79.6-4.8-105.6-13.1c-9.8-3.1-19.4 5.3-17.7 15.3 7.9 47.2 71.3 80 123.3 80s115.3-32.9 123.3-80c1.6-9.8-7.7-18.4-17.7-15.3zm-227.9-57.5c-1 6.2 5.4 11 11 7.9l31.3-16.3 31.3 16.3c5.6 3.1 12-1.7 11-7.9l-6-34.9 25.4-24.6c4.5-4.5 1.9-12.2-4.3-13.2l-34.9-5-15.5-31.6c-2.9-5.8-11-5.8-13.9 0l-15.5 31.6-34.9 5c-6.2.9-8.9 8.6-4.3 13.2l25.4 24.6-6.1 34.9zm259.7-72.7l-34.9-5-15.5-31.6c-2.9-5.8-11-5.8-13.9 0l-15.5 31.6-34.9 5c-6.2.9-8.9 8.6-4.3 13.2l25.4 24.6-6 34.9c-1 6.2 5.4 11 11 7.9l31.3-16.3 31.3 16.3c5.6 3.1 12-1.7 11-7.9l-6-34.9 25.4-24.6c4.5-4.6 1.8-12.2-4.4-13.2z"/>
+    </svg>`;
 
 const opts = {
     background: 'rgba(238, 235, 230, .9)',
@@ -34,26 +22,6 @@ const opts = {
 
 exports.handler = async function(event, context) {
 
-    // if (!init) {
-    //     await initialize(fs.readFileSync(path.join(__dirname, 'svg2png_wasm_bg.wasm')));
-    //     init = true;
-    // }
-
-    // let files = [];
-    // fs.readdirSync(__dirname).forEach(file => {
-    //     files.push(file);
-    // });
-
-    /** @var png Uint8Array */
-    // const png = await svg2png(svg, {
-    //     scale: 2, // optional
-    //     width: 500, // optional
-    //     backgroundColor: 'white', // optional
-    //     fonts: [],
-    //     defaultFontFamily: {},
-    // });
-
-    // let buff = toBuffer(png);
 
     const resvg = new Resvg(svg, opts)
     const pngData = resvg.render()
